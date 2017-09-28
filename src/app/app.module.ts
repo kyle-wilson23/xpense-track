cb34006011b8dbe5ff16a362e7bf578f456fbf56
Kyle Wilson <Kyle Wilson>
2017-09-28 17:06:42 -0400

    initial commit: facebook firebase integration

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Facebook } from '@ionic-native/facebook';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSQ3Qa5VukonXejpMV53Y68yVUtk6U_kM",
+  authDomain: "footprint-8bb10.firebaseapp.com",
+  databaseURL: "https://footprint-8bb10.firebaseio.com",
+  projectId: "footprint-8bb10",
+  storageBucket: "footprint-8bb10.appspot.com",
+  messagingSenderId: "457493383737"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Facebook
+  ]
+})
+export class AppModule { }
